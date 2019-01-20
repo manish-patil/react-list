9c604a84f7d4ee7191953e693353696bd8bc0efc
Manish Patil <manish.patil@hotmail.com>
2019-01-20 14:09:11 +0530

    Added comments

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -4,7 +4,7 @@ export default class ListItem extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
 
-        // console.log("ListItem Constructor executed.", this.props.text, new Date());
+        console.log("ListItem Constructor executed.", this.props.text, new Date());
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -16,7 +16,7 @@ export default class ListItem extends React.Component<any, any> {
     }
 
     render() {
-        // console.log("ListItem render executed.", this.props.text, new Date());
+        console.log("ListItem render executed.", this.props.text, new Date());
 
         return (
             <option value={this.props.value} style={{ backgroundColor: this.props.backgroundColor, color: this.props.foreColor }}>{this.props.text}</ option>
