e5be6d2202186c274ea7d6933cfbd4a45e16b5fe
Manish Patil <manish.patil@hotmail.com>
2019-01-29 14:00:32 +0530

    Updated favicon and ListItem component

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -7,14 +7,6 @@ export default class ListItem extends React.Component<any, any> {
         console.log("ListItem Constructor executed.", this.props.text, new Date());
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        // console.log("ListItem shouldComponentUpdate executed.", nextProps.text, new Date());
-
-        if (this.props.backgroundColor === nextProps.backgroundColor) {
-            return false;
-        }
-    }
-
     render() {
         console.log("ListItem render executed.", this.props.text, new Date());
 
