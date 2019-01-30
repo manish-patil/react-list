b5336415390e4f1f23772a11638cc1b76f80c0a1
Manish Patil <manish.patil@hotmail.com>
2019-01-30 14:56:53 +0530

    Changed ListItem to extent from React.PureComponent

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 
-export default class ListItem extends React.Component<any, any> {
+export default class ListItem extends React.PureComponent<any, any> {
     constructor(props: any) {
         super(props);
 
         console.log("ListItem Constructor executed.", this.props.text, new Date());
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("ListItem shouldComponentUpdate executed.", nextProps.text, new Date());
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log("ListItem shouldComponentUpdate executed.", nextProps.text, new Date());
 
-        if (this.props.backgroundColor === nextProps.backgroundColor) {
-            return false;
-        }
-    }
+    //     if (this.props.backgroundColor === nextProps.backgroundColor) {
+    //         return false;
+    //     }
+    // }
 
     render() {
         console.log("ListItem render executed.", this.props.text, new Date());
